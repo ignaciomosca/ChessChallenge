559530c23c18642684e8da83dd21c5f564ded33d
Ignacio Mosca <ignaciomosca@gmail.com>
2016-08-25 15:34:55 -0300

    Add Tests

--- src/test/java/chess/pieces/QueenTest.java
+++ src/test/java/chess/pieces/QueenTest.java
@@ -10,11 +10,117 @@ import org.junit.Test;
 public class QueenTest {
 
     @Test
-    public void testAttack() throws Exception {
-        Queen queenA = new Queen(1,1);
-        Queen queenB = new Queen(2,2);
+    public void testAttack1() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(7, 7);
         Assert.assertTrue(queenA.attacks(queenB));
     }
+
+    @Test
+    public void testAttack2() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(6,6);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack3() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(4, 6);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack4() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(3, 7);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack5() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(7, 3);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack6() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(6, 4);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack7() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(4, 4);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack8() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(3, 3);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack9() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(6, 5);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack10() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(7,5);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack11() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(4, 5);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack12() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(3, 5);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack13() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(5, 6);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack14() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(5, 7);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack15() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(5, 4);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
+    @Test
+    public void testAttack16() throws Exception {
+        Queen queenA = new Queen(5, 5);
+        Queen queenB = new Queen(5, 3);
+        Assert.assertTrue(queenA.attacks(queenB));
+    }
+
 
     @Test
     public void testDoesNotAttack() throws Exception {
--- src/test/java/chess/pieces/RookTest.java
+++ src/test/java/chess/pieces/RookTest.java
@@ -10,9 +10,58 @@ import org.junit.Test;
 public class RookTest {
 
     @Test
-    public void testAttack() throws Exception {
-        Rook rookA = new Rook(1,1);
-        Rook rookB = new Rook(1,2);
+    public void testAttack1() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(6, 5);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack2() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(7,5);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack3() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(4, 5);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack4() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(3, 5);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack5() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(5, 6);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack6() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(5, 7);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack7() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(5, 4);
+        Assert.assertTrue(rookA.attacks(rookB));
+    }
+
+    @Test
+    public void testAttack8() throws Exception {
+        Rook rookA = new Rook(5, 5);
+        Rook rookB = new Rook(5, 3);
         Assert.assertTrue(rookA.attacks(rookB));
     }
 
